76b638ffb1ee4c362dd560fd1b3f9f64709d58a7
CBorum <christopherborum@gmail.com>
2016-02-17 00:48:29 +0100

    ...

---
+++ src/protocol/ProtocolStrings.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package protocol;
+
+/**
+ *
+ * @author Borum
+ */
+public class ProtocolStrings {
+    public static final String USER = "USER#";
+    public static final String SEND = "SEND#";
+    public static final String LOGOUT = "LOGOUT#";
+    public static final String USERS = "USERS#";
+    public static final String MESSAGE = "MESSAGE#";
+}
