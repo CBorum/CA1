c5dd829616244bdeb3d5fa42123e2a463eb3c32f
PatrickKnowlton <josephinepatrick@Josephines-MacBook-Air.local>
2016-02-17 14:15:07 +0100

    Deleted extra log class

--- src/chatclient/ChatClient.java
+++ src/chatclient/ChatClient.java
@@ -6,7 +6,7 @@
 package chatclient;
 
 import chatserver.ClientHandler;
-import chatserver.Log;
+import log.Log;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
